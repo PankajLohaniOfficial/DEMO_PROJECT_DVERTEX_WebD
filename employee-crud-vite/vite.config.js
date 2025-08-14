7d5c1831e956b8158d4609140b1ade679ffb7678
Babita <Pankajlohani2023@gmail.com>
2025-08-14 15:15:45 +0530

    Update vite.config.js

--- employee-crud-vite/vite.config.js
+++ employee-crud-vite/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/employee-crud-vite/",
+  base: "/DEMO_PROJECT_DVERTEX_WebD/employee-crud-vite", // <-- your repo name here, with leading and trailing slash
 });
